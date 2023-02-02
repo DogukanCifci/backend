a07c796a46a7cdd671d8e5ef28ee0463a51496fc
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2023-02-02 13:23:32 +0100

    realted_name added

--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -7,23 +7,34 @@ from .models import BlogCategory, BlogPost, BlogComments
 class FixSerializer(serializers.ModelSerializer) :
 
     class Meta :
-        exclude=[]
+        exclude=[
+            'created_date',
+            'updated_date'
+        ]
 
 
 #--------------------------Serializers----------------------------#
 class BlogCommentSerializer(FixSerializer) :
+    blog_post = serializers.StringRelatedField()
+    user = serializers.StringRelatedField()
 
     class Meta(FixSerializer.Meta) :
         model = BlogComments
 
 
 class BlogPostSerializer(FixSerializer) :
-
+    # blog_category_id = serializers.IntegerField()
+    blog_category = serializers.StringRelatedField()
+    author = serializers.StringRelatedField()
+    user = serializers.StringRelatedField()
+    blog_comment = BlogCommentSerializer(read_only=True, many=True)
     class Meta(FixSerializer.Meta) :
         model = BlogPost
 
 
 class BlogCategorySerializer(FixSerializer) :
+    user_id = serializers.IntegerField(read_only=True)
+    user = serializers.StringRelatedField()
 
     class Meta(FixSerializer.Meta) :
         model = BlogCategory
